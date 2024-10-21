d8c9ffde2de23a01bc7c32ed5d666341acc23ea3
tim9800 <92989679+tim9800@users.noreply.github.com>
2024-10-21 14:52:39 +1100

    Add resize button and function, background color changing function

--- js/script.js
+++ js/script.js
@@ -2,41 +2,58 @@ const containerMain = document.createElement("div");
 containerMain.setAttribute("id", "container-main");
 document.body.appendChild(containerMain);
 
-for (let i=0; i<16; i++){
-    const gridRow = document.createElement("div");
-    gridRow.setAttribute("class", "grid-row");
-    gridRow.setAttribute("id", "grid-row-" + i);
-    containerMain.appendChild(gridRow);
+initalizeGrid(16);
 
-    for (let j=0; j<16; j++){
-        const gridCell = document.createElement("div");
-        gridCell.setAttribute("class", "grid-element");
-        gridCell.setAttribute("id", "grid-element-" + (i * 16 + j));
-        gridRow.appendChild(gridCell);
-
-        canvas = document.createElement("canvas");
-        canvas.setAttribute("id", "canvas-" + (i * 16 + j));
-        canvas.style.width = "100%";
-        canvas.style.height = "100%";
-        canvas.width = canvas.offsetWidth;
-        canvas.height = canvas.offsetHeight;
-        gridCell.appendChild(canvas);
-    }
-}
-
-containerMain.addEventListener("mousemove", (e) =>
-    {
-        if (e.target.nodeName == "CANVAS") {
-            drawCircle(e);
+containerMain.addEventListener("mouseover", (e) => {
+    if (e.target.classList.contains("grid-element")) {
+        e.target.style.backgroundColor = 'red';
     }
 }
 )
 
-function drawCircle(e) {
-    const canvasTarget = e.target;
-    const ctx = canvasTarget.getContext('2d');
-    ctx.beginPath();
-    ctx.arc(e.pageX, e.pageY, 20, 0, 2 * Math.PI, false);
-    ctx.closePath();
-    ctx.stroke();
+const buttonResizeDiv = document.createElement("div");
+const buttonResize = document.createElement("button");
+buttonResizeDiv.setAttribute("id", "button-resize-div")
+buttonResize.setAttribute("id", "button-resize");
+buttonResize.textContent = "Click me to resize the grid!"
+
+buttonResizeDiv.appendChild(buttonResize);
+document.body.prepend(buttonResizeDiv);
+
+buttonResize.addEventListener("click", (e) => {
+    while (true) {
+        newSize = prompt("How big do you want the new grid to be? (Value must be between 1 and 100)");
+        if (newSize === null) {
+            break;
+        }
+        if (!Number.isInteger(+newSize)) {
+            alert("Please enter a whole number between 1 and 100.");
+            continue;
+        }
+        if (!(+newSize >= 1 && +newSize <= 100)) {
+            alert("Your number is not between 1 and 100. Please try again.");
+            continue;
+        }
+        break;
+    }
+    
+    containerMain.innerHTML = "";
+    initalizeGrid(newSize);
+})
+
+function initalizeGrid(size) {
+    for (let i=0; i<size; i++){
+        const gridRow = document.createElement("div");
+        gridRow.setAttribute("class", "grid-row");
+        gridRow.setAttribute("style", "width:960px");
+        gridRow.setAttribute("style", `height:${960/size}px`);
+        containerMain.appendChild(gridRow);
+    
+        for (let j=0; j<size; j++){
+            const gridCell = document.createElement("div");
+            gridCell.setAttribute("class", "grid-element");
+            gridCell.setAttribute("style", `width:${960/size}px`)
+            gridRow.appendChild(gridCell);
+        }
+    }
 }
